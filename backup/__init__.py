c36d45bff8e9359d9c5f8156884b9504660b8615
Wan2405 <quananh123@gmail.com>
2026-03-15 14:06:59 +0700

    chore: remove deprecated files and empty folders

--- backup/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Package chứa các module xử lý cho Auto Grader
-"""
-
-from .log_processor import LogProcessor
-from .error_classifier import ErrorClassifier
-
-__all__ = ['LogProcessor', 'ErrorClassifier']
